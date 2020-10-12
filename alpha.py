9638d9aaf0592a1be4c9f78b1d8ba8e3ab56c2d0
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2020-10-12 15:03:47 +0530

    Added scapping from nse

--- alpha.py
+++
@@ -1,6 +0,0 @@
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
-ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
-data, meta_data = ts.get_intraday(
-    symbol='GOOGL', interval='day', outputsize='full')
-pprint(data.head(20))
