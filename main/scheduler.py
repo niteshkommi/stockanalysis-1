9dbc582a124fa6930c2df3c993b53e4e5f48f675
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2021-01-05 17:00:35 +0530

    Changed Date and Signal Date field

--- main/scheduler.py
+++ main/scheduler.py
@@ -5,5 +5,5 @@ from .views import Computation
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(Computation, 'cron', hour='06', minute='00')
+    scheduler.add_job(Computation, 'cron', hour='11', minute='40')
     scheduler.start()
