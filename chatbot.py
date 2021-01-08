954a98dd29136823e7976abccb30fe6eea44dc83
niteshkommi <67100794+niteshkommi@users.noreply.github.com>
2021-01-08 18:11:34 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,26 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
-bot=ChatBot(name='chat',logic_adapters=['chatterbot.logic.TimeLogicAdapter',
-                                 'chatterbot.logic.BestMatch','chatterbot.logic.SpecificResponseAdapter'])
-trainer=ChatterBotCorpusTrainer(bot)
-trainer.train(
-    "chatterbot.corpus.english.greetings",
-    "chatterbot.corpus.english.conversations",
-    "chatterbot.corpus.english.money"
+
+# Creating ChatBot Instance
+chatbot = ChatBot('StockBot')
+
+ # Training with Personal Ques & Ans
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+
+# Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+trainer_corpus.train(
+    'chatterbot.corpus.english'
 )
-print('Bot: hi,and welcome! ask me something')
-while True:
-    inp=input('\nYou :')
-    if inp=='done':
-        break
-    else:
-        resp=bot.get_response(inp)
-        print('\nBot :',resp)
